78370ebec0818133f3be864b678a80d27599bb69
Ziauddin Sameer <ziauddinsameer@gmail.com>
2021-11-06 20:35:30 +0530

    sm8250-common: Udfps: Add show|hide udfps view callbacks to IBiometricsFingerprint@2.3
    Co-authored-by: Kunal Shah <kunalshah912@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -32,7 +32,7 @@
 #define OP_FINISH_FP_ENROLL 10
 
 #define OP_DISPLAY_NOTIFY_PRESS 9
-
+#define OP_DISPLAY_AOD_MODE 8
 #define OP_DISPLAY_SET_DIM 10
 
 namespace android {
@@ -90,6 +90,18 @@ Return<void> BiometricsFingerprint::onFingerUp() {
     mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onShowUdfpsOverlay() {
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onHideUdfpsOverlay() {
+    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+    mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
+    mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
     return Void();
 }
 
